84e32c56d080a3f7428ab1bbc4c4ac22e65f8919
vvitordantas <40401677+vvitordantas@users.noreply.github.com>
2025-09-20 00:12:34 -0300

    Mytv

--- redirects.js
+++ redirects.js
@@ -3,14 +3,15 @@
 const links = {
   // Apelido: 'URL de destino completa'
   // Lembre-se de trocar pelos seus dados!
-  
+
+  'mytv': 'http://cx05d.sbs',
   'github': 'https://github.com/seu-usuario',
   'linkedin': 'https://www.linkedin.com/in/seu-perfil/',
   'portfolio': 'https://seu-website.com',
   'yt': 'https://www.youtube.com/c/seu-canal',
   'cv': 'https://link-para-seu-curriculo.com/cv.pdf'
-  'mytv': 'http://cx05d.sbs',
   
   // Adicione quantos links quiser aqui, sempre com uma vírgula no final (exceto no último)
 
 };
+
