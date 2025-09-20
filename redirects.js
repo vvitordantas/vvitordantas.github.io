e40f090b0d53bf651b0599f2ce04e105a5fa7b85
vvitordantas <40401677+vvitordantas@users.noreply.github.com>
2025-09-19 23:30:54 -0300

    mytv

--- redirects.js
+++ redirects.js
@@ -9,6 +9,8 @@ const links = {
   'portfolio': 'https://seu-website.com',
   'yt': 'https://www.youtube.com/c/seu-canal',
   'cv': 'https://link-para-seu-curriculo.com/cv.pdf'
+  'mytv': 'http://cx05d.sbs',
   
   // Adicione quantos links quiser aqui, sempre com uma vírgula no final (exceto no último)
+
 };
